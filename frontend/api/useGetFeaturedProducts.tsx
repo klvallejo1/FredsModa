ccb6a9f4fb9d63c5c9d8865fd9c6644513ea0eb6
klvallejo1 <leandrohernandez1903@gmail.com>
2025-01-22 14:17:49 -0500

    Back funcionando

--- frontend/api/useGetFeaturedProducts.tsx
+++ frontend/api/useGetFeaturedProducts.tsx
@@ -1,31 +1,27 @@
 "use client"
 import { useEffect, useState } from "react";
-import { ProductType } from "@/types/product";
 
 export function useGetFeaturedProducts() {
-    const [result, setResult] = useState([]);
+    const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/products?filters[isFeatured][$eq]=true&populate=*`
+    const [result, setResult] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState("");
 
     useEffect(() => {
-        fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/products?filters[isFeatured][$eq]=true&populate=*`)
-            .then(res => {
-                console.log("Status:", res.status);
-                return res.json();
-            })
-            .then(data => {
-                console.log("Datos:", data);
-                if (data?.data) {
-                    setResult(data.data);
-                }
-                setLoading(false);
-            })
-            .catch(error => {
-                console.error("Error:", error);
-                setError(error.message);
-                setLoading(false);
-            });
-    }, []);
+        (async() => {
+            try{
+                const res = await fetch(url)
+                const json = await res.json()
+                setResult(json.data)
+                setLoading(false)
+
+            }catch (error : any){
+                setError(error)
+                setLoading(false)
+            }
+        })()
+       
+    }, [url]);
 
     return { loading, result, error };
 }
--- frontend/components/featured-product.tsx
+++ frontend/components/featured-product.tsx
@@ -1,63 +1,43 @@
 "use client";
+
 import { useGetFeaturedProducts } from "@/api/useGetFeaturedProducts";
 import { ResponseType } from "@/types/response";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "./ui/carousel";
+import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
 import SkeletonSchema from "./skeletonSchema";
 import { ProductType } from "@/types/product";
 import { Card, CardContent } from "./ui/card";
-import { Expand, ShoppingCart } from "lucide-react";
-import IconButton from "./icon-button";
-import { useRouter } from "next/navigation";
 
 const FeaturedProducts = () => {
-  const { loading, result, error } = useGetFeaturedProducts();
-  const router = useRouter();
+  const { result, loading }: ResponseType = useGetFeaturedProducts();
 
   return (
     <div className="max-w-6xl py-4 mx-auto sm:py-16 sm:px-24">
       <h3 className="px-6 text-3xl sm:pb-8">Productos Destacados</h3>
       <Carousel>
-        <CarouselContent className="-ml-2 md:-ml-4">
+        <CarouselContent className="-ml-2 md:-ml4">
           {loading && <SkeletonSchema grid={3} />}
+          {result !== null &&
+            result.map((product: ProductType) => {
+              const { attributes, id } = product;
+              const {slug, images, productName, size, origin} = attributes
 
-          {result &&
-            result.map((product) => (
-              <CarouselItem
-                key={product.id}
-                className="md:basis-1/2 lg:basis-1/3 group"
-              >
-                <div className="p-1">
-                  <Card className="py-4 border-gray-200 shadow-none">
-                    <CarouselContent className="relative flex items-center justify-center px-6 py-2">
-                      <img
-                        src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${
-                          product.images[0]?.formats?.small?.url ?? ""
-                        }`}
-                        alt={product.productName ?? "Producto"}
-                      />
-                      <div className="absolute w-full px-6 transition duration-200 opacity-0 group-hover:opacity-100 bottom-5">
-                        <div className="flex justify-center gap-x-6">
-                          <IconButton
-                            onClick={() =>
-                              router.push(`productos/${product.slug}`)
-                            }
-                            icon={<Expand size={20} />}
-                            className="text-gray-600"
-                          />
-                          <ShoppingCart />
-                        </div>
-                      </div>
-                    </CarouselContent>
-                  </Card>
-                </div>
-              </CarouselItem>
-            ))}
+              return (
+                <CarouselItem
+                  key={id}
+                  className="md:basis-1/2 lg:basis-1/3 group"
+                >
+                  <div className="p-1">
+                    <Card className="py-4 border-gray-200 shadow-none">
+                      <CardContent className="relative flex items-center justify-center px-6 py-2">
+                        <img 
+                        src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${images.data[0].attributes.url}`} alt="Featured" />
+                      
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              );
+            })}
         </CarouselContent>
       </Carousel>
     </div>
